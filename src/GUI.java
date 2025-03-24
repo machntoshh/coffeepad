d734002715fb40157eba474750520cf2803dfd43
machntoshh <erimgomess@gmail.com>
2025-03-24 19:11:03 -0300

    Adicionando alguns outros recursos como barra de menu | por gentileza verificar canal https://www.youtube.com/@RyiSnow

--- src/GUI.java
+++ src/GUI.java
@@ -6,16 +6,19 @@ public class GUI {
     JFrame window; // parece ser imports do Swing, essas coisas com J e talz
     JTextArea textArea;
     JScrollPane scrollPane;
+    JMenu menuFile, menuEdit, menuFormat, menuColor;
+    JMenuBar menuBar;
+    JMenuItem itemNew, itemOpen, itemSave, itemSaveAs, itemExit;
 
     public static void main(String[] args) {
         new GUI();
     }
-    public GUI() { // constructor
-        // vou chamar o metodo createWindow nesse constructor
-        createWindow();
-        // vou chamar o createTextArea agora
-        createTextArea();
 
+    public GUI() { // constructor
+        createWindow(); // vou chamar o metodo createWindow nesse constructor e quando for criando os outros
+        createTextArea();
+        createMenuBar();
+        createFileMenu();
         window.setVisible(true); // setando a visibilidade dela para true
     }
 
@@ -31,9 +34,41 @@ public class GUI {
         textArea = new JTextArea();
         scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER); // Add barra de rolagem
         scrollPane.setBorder(BorderFactory.createEmptyBorder()); // tirando as mini boridnhas no notepad
-
         window.add(scrollPane, BorderLayout.CENTER); // add janela com scrollpane centralizada
-
         // window.add(textArea);  add o textArea para a window
+    }
+
+    public void createMenuBar() { // nome diz tudo
+        menuBar = new JMenuBar();
+        window.setJMenuBar(menuBar);
+
+        menuFile = new JMenu("File");
+        menuBar.add(menuFile); // add o novo criado menuFile na menuBar, navegação de arquivos
+
+        menuEdit = new JMenu("Edit");
+        menuBar.add(menuEdit); // editar arquivos
+
+        menuFormat = new JMenu("Format text");
+        menuBar.add(menuFormat); // formatar texto
+
+        menuColor = new JMenu("Color");
+        menuBar.add(menuColor); // trocar a cor
+    }
+
+    public void createFileMenu() {
+        itemNew = new JMenuItem("New file");
+        menuFile.add(itemNew);
+
+        itemOpen = new JMenuItem("Open file");
+        menuFile.add(itemOpen);
+
+        itemSave = new JMenuItem("Save file");
+        menuFile.add(itemSave);
+
+        itemSaveAs = new JMenuItem("Save file as");
+        menuFile.add(itemSaveAs);
+
+        itemExit = new JMenuItem("Exit file");
+        menuFile.add(itemExit);
     }
 }
